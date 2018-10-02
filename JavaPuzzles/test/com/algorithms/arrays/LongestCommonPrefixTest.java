bb971c2530ded4baceab01f587d3de4fa54786d6
ganesh.me@gmail.com <ganesh.me@gmail.com>
2018-10-02 14:27:03 +0530

    fixed compilation, moved test to tests folder

--- JavaPuzzles/test/com/algorithms/arrays/LongestCommonPrefixTest.java
+++ JavaPuzzles/test/com/algorithms/arrays/LongestCommonPrefixTest.java
@@ -10,13 +10,13 @@ public class LongestCommonPrefixTest {
     @Test
     public void test() {
        String prefix =  LongestCommonPrefix.longestCommonPrefix(new String[]{"flower","flow","flight"});
-        Assert.assertEquals("fl", prefix);
+        assertEquals("fl", prefix);
 
         prefix =  LongestCommonPrefix.longestCommonPrefix(new String[]{"flo","flo","flo"});
-        Assert.assertEquals("flo", prefix);
+        assertEquals("flo", prefix);
 
         prefix =  LongestCommonPrefix.longestCommonPrefix(new String[]{"flower","flo","flo"});
-        Assert.assertEquals("flo", prefix);
+        assertEquals("flo", prefix);
     }
 
 }
